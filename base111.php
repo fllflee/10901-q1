9c66e261c6abdb79803f99c21fa2ccad9cf10539
fllf <fllf@fllf.com>
2020-12-01 16:08:36 +0800

    完成base

--- base111.php
+++ base111.php
@@ -8,7 +8,7 @@ class db{
 	private $pdo;
 
 	//設定建構式
-	public function __construct(){
+	public function __construct($table){
 		$this->table=$table;
 		$this->pdo=new PDO($this->dsn,$this->root,$this->password);
 	}
@@ -74,6 +74,31 @@ class db{
 	}
 
 	//新增/更新資料
+	public function save($arg){
+		if(!empty($arg['id'])){
+			//更新
+			//update $this->table set xxx=yyy where `id`='xxx'
+			foreach($arg as $key => $value){
+				if($key!='id'){
+					$tmp[]=sprintf("`%s`='%s'",$key,$value);
+
+				}
+			}
+			$sql="update $this->table set ".implode(",",$tmp)." where `id`='".$arg['id']."'";
+		
+
+		}else{
+			//新增
+			//insert into $this->table (``,``,``) values('','','')
+			$sql="insert into $this->table (`".implode("`,`",array_keys($arg))."`) values('".implode("','",$arg)."')";
+
+		}
+
+		return $this->pdo->exec($sql);
+
+	}
+
+
 
 	//刪除資料
 	public function del($arg){
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "base.php";
+include_once "base111.php";
 
 $total=new db('total');
 
